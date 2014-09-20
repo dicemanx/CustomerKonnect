b6da9c9212e43ada0e8851d8a96ece8713bf0cd6
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-09-20 22:38:50 +0530

    bootswatch and feedback display

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -61,6 +61,10 @@ class BusinessesController < ApplicationController
     end
   end
 
+  def feedback
+      @feedback = Feedback.where(business_id: params[:id])
+      
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_business
