262b951a6e3839105e2cbaf93dcd3f60efbc1f52
Raghav Malik <raghav1987@gmail.com>
2014-09-20 17:54:41 +0530

    Fixed merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 CustomerKonnect::Application.routes.draw do
-<<<<<<< HEAD
   root :to => "shoppers#index", as: "home", via: "get"
   match '/businesses', to: 'businesses#index', via: "get", as: :business
-  match '/businesses', to: 'businesses#index', via: "patch", as: :business
+  match '/businesses', to: 'businesses#index', via: "patch", as: :business_patch
   match '/businesses/show/:id', to: 'businesses#show', via: "get",as: :show_business
   match '/businesses/new', to: 'businesses#new', via: "get"
   match '/businesses/:id/edit', to: 'businesses#edit', via: "get", as: :edit_business
